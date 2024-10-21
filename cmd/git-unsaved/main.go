87cf33b9e68fb676358e729b2bd0e5c53175b8d3
Klaus Hartl <klaus.hartl@gmail.com>
2024-10-21 16:49:46 +0200

    Implement list UI for showing repos

--- cmd/git-unsaved/main.go
+++ cmd/git-unsaved/main.go
@@ -1,66 +1,125 @@
 package main
 
 import (
-	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/urfave/cli/v2"
 )
 
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type repoMsg struct {
+	repo repo
+}
+type doneMsg struct{}
 type errMsg error
 
+type repo struct {
+	path string
+}
+
+func (r repo) Title() string {
+	return r.path
+}
+
+func (r repo) Description() string {
+	return "Foo" // TODO: Gather Git status here, separated by \n
+}
+
+func (r repo) FilterValue() string {
+	return r.path
+}
+
 type model struct {
-	spinner  spinner.Model
+	list     list.Model
+	sub      chan repoMsg
 	quitting bool
 	err      error
 }
 
-func initialModel() model {
-	s := spinner.New()
-	s.Spinner = spinner.MiniDot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("86"))
-	return model{spinner: s}
-}
-
 func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+	return tea.Batch(
+		m.list.StartSpinner(),
+		getRepos(m.sub),
+		waitForRepoStatus(m.sub),
+	)
 }
 
+// TODO: upon selection + enter, cd to directory!
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "ctrl+c", "q":
 			m.quitting = true
 			return m, tea.Quit
-		default:
-			return m, nil
 		}
-
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	case repoMsg:
+		return m, tea.Sequence(
+			m.list.InsertItem(len(m.list.Items()), msg.repo),
+			waitForRepoStatus(m.sub), // wait for next found repo
+		)
+	case doneMsg:
+		m.list.StopSpinner()
+		return m, nil
 	case errMsg:
 		m.err = msg
 		return m, nil
-
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
 	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n\n%s Scanning Git repositories...\n\n", m.spinner.View())
+	s := m.list.View()
 	if m.quitting {
-		return str + "\n"
+		s += "\n"
 	}
-	return str
+	return s
+}
+
+func getRepos(sub chan repoMsg) tea.Cmd {
+	return func() tea.Msg {
+		err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				// TODO: filter for git repos
+				abspath, err := filepath.Abs(path)
+				if err != nil {
+					return err
+				}
+				sub <- repoMsg{repo: repo{path: abspath}}
+			}
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+		return doneMsg{}
+	}
+}
+
+func waitForRepoStatus(sub chan repoMsg) tea.Cmd {
+	return func() tea.Msg {
+		return repoMsg(<-sub)
+	}
 }
 
 func main() {
@@ -83,7 +142,13 @@ Flags:
 		Usage:   "Find all your dirty Git repositories",
 		Version: "v0.0.1",
 		Action: func(*cli.Context) error {
-			p := tea.NewProgram(initialModel())
+			l := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
+			l.Title = "Repositories"
+			l.SetSpinner(spinner.MiniDot)
+			l.ToggleSpinner()
+			m := model{list: l, sub: make(chan repoMsg)}
+			// TODO: how to pass along path argument?
+			p := tea.NewProgram(m, tea.WithAltScreen())
 			if _, err := p.Run(); err != nil {
 				return err
 			}
--- go.mod
+++ go.mod
@@ -9,8 +9,10 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 )
 
