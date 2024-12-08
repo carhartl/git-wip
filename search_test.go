57142bb8fa614d9b68e1ed0b8fea86a0b16ede0a
Klaus Hartl <klaus.hartl@gmail.com>
2024-12-08 09:12:26 +0100

    Add test for detecting a Git repo

---
+++ search_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDetectGitRepo(t *testing.T) {
+	d, err := os.MkdirTemp(os.TempDir(), "test")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(d)
+	cmd := exec.Command("git", "init")
+	cmd.Dir = d
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+	err = withUntrackedFile(d)
+	if err != nil {
+		t.Error(err)
+	}
+
+	output := make(chan repo)
+	go func() {
+		getRepos(d, output)()
+		defer close(output)
+	}()
+
+	select {
+	case result := <-output:
+		require.Equal(t, d, result.path)
+	case <-time.After(2 * time.Second):
+		t.Fatal("Test timed out")
+	}
+}
