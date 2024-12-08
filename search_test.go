5da7785be690b51fbf0cd45070a5f85d4facf82d
Klaus Hartl <klaus.hartl@gmail.com>
2024-12-08 10:05:13 +0100

    Add test for search w/ excluded directories

--- search_test.go
+++ search_test.go
@@ -35,3 +35,50 @@ func TestDetectGitRepo(t *testing.T) {
 		t.Fatal("Test timed out")
 	}
 }
+
+func TestExcludedDirectory(t *testing.T) {
+	d, err := os.MkdirTemp(os.TempDir(), "test")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(d)
+	err = os.MkdirAll(d+"/.hidden/test", 0750)
+	if err != nil {
+		t.Error(err)
+	}
+	cmd := exec.Command("git", "init")
+	cmd.Dir = d + "/.hidden/test"
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.MkdirAll(d+"/node_modules/test", 0750)
+	if err != nil {
+		t.Error(err)
+	}
+	cmd = exec.Command("git", "init")
+	cmd.Dir = d + "/node_modules/test"
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	output := make(chan repo)
+	go func() {
+		_ = collectDirtyRepos(d, output)
+		defer close(output)
+	}()
+
+	detected := 0
+	select {
+	case result := <-output:
+		// Closed channel returning the zero value of the underlying type, thus check non-emptyness..
+		if result.path != "" {
+			detected++
+		}
+	case <-time.After(2 * time.Second):
+		t.Fatal("Test timed out")
+	}
+
+	require.Equal(t, 0, detected)
+}
