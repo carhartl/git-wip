d47bd5a1c8d84590e67e7e662cfd53c95fc1d59b
Klaus Hartl <klaus.hartl@gmail.com>
2026-03-20 11:41:11 +0100

    Make separate test for quitting

--- tui_test.go
+++ tui_test.go
@@ -49,12 +49,10 @@ func TestSearchOutput(t *testing.T) {
 		return bytes.Contains(bts, []byte("1 file to commit"))
 	}, teatest.WithCheckInterval(time.Millisecond*100), teatest.WithDuration(time.Second*3))
 
-	tm.Send(tea.KeyPressMsg{
-		Text: "q",
-		Code: 'q',
-	})
-
-	tm.WaitFinished(t, teatest.WithFinalTimeout(time.Second))
+	err = tm.Quit()
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestUpdate(t *testing.T) {
@@ -94,6 +92,32 @@ func TestUpdate(t *testing.T) {
 	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
 		return bytes.Contains(bts, []byte("1 file to commit, missing upstream"))
 	}, teatest.WithCheckInterval(time.Millisecond*100), teatest.WithDuration(time.Second*3))
+
+	err = tm.Quit()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestQuit(t *testing.T) {
+	d, err := os.MkdirTemp(os.TempDir(), "test")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		_ = os.RemoveAll(d)
+	}()
+	cmd := exec.Command("git", "init")
+	cmd.Dir = d
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	tm := teatest.NewTestModel(t, initialModel(d), teatest.WithInitialTermSize(300, 100))
+	if err != nil {
+		t.Error(err)
+	}
 
 	tm.Send(tea.KeyPressMsg{
 		Text: "q",
